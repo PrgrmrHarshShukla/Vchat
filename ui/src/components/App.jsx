dbf83e0e5790cf221d9d7b3ee6a0fa0c8e195e9a
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-09-08 19:11:19 +0530

    Backend Integration attempt.

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import io from "socket.io-client";
-const socket = io("http://localhost:5000");
+const socket = io(process.env.REACT_APP_API_URL);
 
 import './App.css';
 import Chat from './Chat';
