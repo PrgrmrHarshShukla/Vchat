2e70c709ba801a92eb1360a68deda472b8ef5976
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-09-10 07:43:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const http = require("http");
 const socketIo = require("socket.io");
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
-        res.status(200).send("Welcome");
+        res.json({
+            msg: "Welcome"
+        });
     }
 });
 
